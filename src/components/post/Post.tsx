193a3087dd2cf0b1098ac1217da643d54adab29d
cacomeatball <jkaviau2@gmail.com>
2026-01-09 21:17:06 +0800

    Added stuff.

---
+++ src/components/post/Post.tsx
@@ -0,0 +1,71 @@
+import "./post.css"
+
+export default function Post() {
+  return (
+    <div className="post">
+        <img 
+            className="postImg"
+            src="https://pbs.twimg.com/media/EWnlvZCUcAk0oAs?format=jpg&name=small" 
+            alt=""
+        />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Hello</span>
+                <span className="postCat">World</span>
+            </div>
+            <span className="postTitle">
+                Lorem ipsum dolor, sit amet
+            </span>
+            <hr />
+            <span className="postDate">
+                67 hours ago
+            </span>
+            <p className="postDesc">
+                What you need is my Stand, "The World".
+
+What you can find beyond the powers of my Stand is where you need to go in order to find Heaven. What you need is a trustworthy friend. He must be someone capable of controlling his own desires. He must be someone who is not interested in political power, fame, wealth, or sexual desire, and who chooses the will of God before the law of humans.
+
+Will I, DIO, be able to meet someone like this one day? What I also need is the lives of more than 36 humans who have sinned, because those who have sinned harbor a strong power within.
+
+There are 14 phrases that one must keep in mind:
+
+Spiral staircase (らせん階段 Rasen Kaidan)
+
+Rhinoceros beetle (カブト虫 Kabutomushi)
+
+Ruins street (廃墟の街 Haikyo no machi)
+
+Fig tart (イチジクのタルト Ichijiku no taruto)
+
+Rhinoceros beetle (カブト虫 Kabutomushi)
+
+Via dolorosa (ドロローサへの道 Dororōsa e no michi)
+
+Rhinoceros beetle (カブト虫 Kabutomushi)
+
+Singularity point (特異点 Tokuiten)
+
+Giotto (ジョット Jotto)
+
+Angel (天使エンジェル Enjeru)
+
+Hydrangea (紫陽花 Ajisai)
+
+Rhinoceros beetle (カブト虫 Kabutomushi)
+
+Singularity point (特異点 Tokuiten)
+
+Secret emperor (秘密の皇帝 Himitsu no Kōtei)
+
+I'll engrave these words onto my Stand so I won't forget them. What is most necessary is "courage"; I must have the courage to destroy my Stand momentarily. As it disintegrates, my Stand will absorb the souls of the 36 sinners and will give birth to something utterly new. Whatever is born will "awaken". It will show interest in the 14 words that my trusted friend will utter... My friend will trust me and I will become his "friend".
+
+Lastly, I need an appropriate location. North latitude, 28 degrees, 24 minutes, West longitude 80 degrees, 36 minutes...
+
+Go there and wait for the New Moon...
+
+That's when Heaven will come.
+            </p>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/singlePost/SinglePost.tsx
@@ -0,0 +1,71 @@
+import "./singlepost.css"
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+        <div className="singlePostWrapper">
+            <img 
+                src="https://info-ongeki.sega.jp/wp-content/uploads/2022/07/b38cc13a9ca11623c1826f3341634b5f.jpg" 
+                alt="" 
+                className="singlePostImg" 
+            />
+            <h1 className="singlePostTitle">
+                Lorem ipsum dolor, sit amet
+                <div className="singlePostEdit">
+                    <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
+                    <i className="singlePostIcon fa-regular fa-trash-can"></i>
+                </div>
+            </h1>
+            <div className="singlePostInfo">
+                <span className="singlePostAuthor">Author: <b>cacomeatball.</b></span>
+                <span className="singlePostDate">67 hours ago</span>
+            </div>
+            <p className="singlePostDesc">
+                What you need is my Stand, "The World".
+
+What you can find beyond the powers of my Stand is where you need to go in order to find Heaven. What you need is a trustworthy friend. He must be someone capable of controlling his own desires. He must be someone who is not interested in political power, fame, wealth, or sexual desire, and who chooses the will of God before the law of humans.
+
+Will I, DIO, be able to meet someone like this one day? What I also need is the lives of more than 36 humans who have sinned, because those who have sinned harbor a strong power within.
+
+<br />There are 14 phrases that one must keep in mind:
+
+<br />Spiral staircase (らせん階段 Rasen Kaidan)
+
+<br />Rhinoceros beetle (カブト虫 Kabutomushi)
+
+<br />Ruins street (廃墟の街 Haikyo no machi)
+
+<br />Fig tart (イチジクのタルト Ichijiku no taruto)
+
+<br />Rhinoceros beetle (カブト虫 Kabutomushi)
+
+<br />Via dolorosa (ドロローサへの道 Dororōsa e no michi)
+
+<br />Rhinoceros beetle (カブト虫 Kabutomushi)
+
+<br />Singularity point (特異点 Tokuiten)
+
+<br />Giotto (ジョット Jotto)
+
+<br />Angel (天使エンジェル Enjeru)
+
+<br />Hydrangea (紫陽花 Ajisai)
+
+<br />Rhinoceros beetle (カブト虫 Kabutomushi)
+
+<br />Singularity point (特異点 Tokuiten)
+
+<br />Secret emperor (秘密の皇帝 Himitsu no Kōtei)
+
+<br />I'll engrave these words onto my Stand so I won't forget them. What is most necessary is "courage"; I must have the courage to destroy my Stand momentarily. As it disintegrates, my Stand will absorb the souls of the 36 sinners and will give birth to something utterly new. Whatever is born will "awaken". It will show interest in the 14 words that my trusted friend will utter... My friend will trust me and I will become his "friend".
+
+Lastly, I need an appropriate location. North latitude, 28 degrees, 24 minutes, West longitude 80 degrees, 36 minutes...
+
+Go there and wait for the New Moon...
+
+<br />That's when Heaven will come.﻿
+            </p>
+        </div>
+    </div>
+  )
+}
