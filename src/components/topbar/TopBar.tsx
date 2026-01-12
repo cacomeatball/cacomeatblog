57f8c693694275a69f53f46f694205399ed428a6
cacomeatball <jkaviau2@gmail.com>
2026-01-12 14:30:44 +0800

    Framework done.

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,6 +1,8 @@
 import "./topbar.css"
+import { Link } from "react-router-dom";
 
 export default function topbar() {
+    const user = false;
   return (
     <div className="top">
         <div className="topLeft">
@@ -10,19 +12,42 @@ export default function topbar() {
         </div>
         <div className="topCenter">
             <ul className="topList">
-                <li className="topListItem">Home</li>
-                <li className="topListItem">About</li>
-                <li className="topListItem">Contact</li>
-                <li className="topListItem">Write</li>
-                <li className="topListItem">Logout</li>
+                <li className="topListItem">
+                    <Link className="link" to="/">Home</Link>
+                </li>
+                <li className="topListItem">
+                    <Link className="link" to="/">About</Link>
+                </li>
+                <li className="topListItem">
+                    <Link className="link" to="/">Contact</Link>
+                </li>
+                <li className="topListItem">
+                    <Link className="link" to="/write">Write</Link>
+                </li>
+                <li className="topListItem">
+                    {user && "Logout"}
+                </li>
             </ul>
         </div>
         <div className="topRight">
-            <img
-                className="topImg"
-                src="https://pbs.twimg.com/media/G9joRN3bUAAau9-?format=jpg&name=small"
-                alt=""
-            />
+            {
+                user ? (
+                    <img
+                        className="topImg"
+                        src="https://pbs.twimg.com/media/G9joRN3bUAAau9-?format=jpg&name=small"
+                        alt=""
+                    />
+                 ) : (
+                    <ul className="topList">
+                        <li className="topListItem">
+                            <Link className="link" to="/login">Login</Link>
+                        </li>
+                        <li className="topListItem">
+                            <Link className="link" to="/register">Register</Link>
+                        </li>
+                    </ul>
+                 )
+            }
             <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
         </div>
     </div>
--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -5,8 +5,36 @@ export default function Settings() {
   return (
     <div className="settings">
         <div className="settingsWrapper">
-            <Sidebar />
+            <div className="settingsTitle">
+                <span className="settingsUpdateTitle">
+                    Update Your Account
+                </span>
+                <span className="settingsDeleteTitle">
+                    Delete Account
+                </span>
+            </div>
+            <form action="" className="settingsForm">
+                <label>Profile Picture</label>
+                <div className="settingsPP">
+                    <img 
+                        src="https://pbs.twimg.com/media/G9joRN3bUAAau9-?format=jpg&name=small" 
+                        alt=""
+                    />
+                    <label htmlFor="fileInput">
+                        <i className="settingsPPIcon fa-regular fa-user"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display:"none"}}/>
+                </div>
+                <label>Username</label>
+                <input type="text" placeholder="Josuke" />
+                <label>Email</label>
+                <input type="email" placeholder="josuke@morioh.com" />
+                <label>Password</label>
+                <input type="password" />
+                <button className="settingsSubmit">Update</button>
+            </form>
         </div>
+        <Sidebar />
     </div>
   )
 }
